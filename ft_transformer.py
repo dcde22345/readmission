fe93136d26266adabaa331005082c69413a2b1da
dcde22345 <dcde22345@gmail.com>
2025-05-29 20:07:19 +0800

    dev

--- ft_transformer.py
+++ ft_transformer.py
@@ -1,4 +1,4 @@
-# %%
+
 import numpy as np
 from tqdm import tqdm
 
@@ -17,276 +17,245 @@ from torchkeras.tabular.models import FTTransformerConfig, FTTransformerModel
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
-np.random.seed(42)
-train_df = pd.read_csv("data/train_df.csv")
+from imblearn.over_sampling import SMOTE
 
-train_df.head()
+np.random.seed(42)
 
 # ----- 數值型與類別型特徵 -----
-numerical_features = ['age']
-categorical_features = ['gender', 'primary_diagnosis', 'discharge_to', 'num_procedures', 'days_in_hospital', 'comorbidity_score']
-target = 'readmitted'
+NUMERICAL_FEATURES = ['age']
+CATEGORICAL_FEATURES = ['gender', 'primary_diagnosis', 'discharge_to', 'num_procedures', 'days_in_hospital', 'comorbidity_score']
+TARGET = 'readmitted'
+NUMERICAL_COLUMNS = [f'num_{i}' for i in range(len(NUMERICAL_FEATURES))]
+CATEGORICAL_COLUMNS = [f'cat_{i}' for i in range(len(CATEGORICAL_FEATURES))]
 
-# %%
-df = train_df.copy()
+class CustomerFTTransformer:
+    def __init__(self):
+        self.scaler = StandardScaler()
+        self.train_df = pd.read_csv("data/train_df.csv")
+        self.test_df_test_csv = pd.read_csv("data/test_df.csv")
 
-# %% [markdown]
-# # 前處理
+        # 使用 TabularPreprocessor 進行預處理
+        self.preprocessor = TabularPreprocessor(
+            cat_features=CATEGORICAL_COLUMNS,  # 類別特徵
+            numeric_features=NUMERICAL_COLUMNS,    # 數值特徵
+            normalization='standard',           # 數值特徵標準化（可選：'minmax' 或 None）
+            onehot_max_cat_num=1,
+        )
 
-# %%
-# 數值標準化
-scaler = StandardScaler()
-df[numerical_features] = scaler.fit_transform(df[numerical_features])
+        self.model_config = FTTransformerConfig(
+            # ModelConfig 參數
+            task="classification",  # 二元分類
+            num_attn_blocks=3,
+        )
 
-# 將類別特徵轉為 category 類型
-for col in categorical_features:
-    df[col] = df[col].astype('category')
+        self.model = None
+        self.config = None
+        self.train_df_train_csv_processed = None
+        self.test_df_train_csv_processed = None
+        self.train_target_df = None
+        self.test_target_df = None
+        self.train_df_train_csv = None
+        self.test_df_train_csv = None
+        self.numerical_features_processed = None
+        self.embedding_features_processed = None
+        self.ds_train = None
+        self.ds_test = None
+        self.dl_train = None
+        self.dl_test = None
 
-df[target] = df[target].astype('category')
+    def format_dataframe(self):
+        # 將numerical做standarize
+        self.train_df[NUMERICAL_FEATURES] = self.scaler.fit_transform(self.train_df[NUMERICAL_FEATURES])
+        # 將類別特徵轉為 category 類型
+        for col in CATEGORICAL_FEATURES:
+            self.train_df[col] = self.train_df[col].astype('category')
 
-# 轉成 numpy
-X_num = df[numerical_features].values.astype(np.float32)
-X_cat = df[categorical_features].values
-y = df[target].values.astype(np.int64)
-
-# 分割訓練測試資料
-X_num_train, X_num_test, X_cat_train, X_cat_test, y_train, y_test = train_test_split(
-    X_num, X_cat, y, test_size=0.2, random_state=42
-)
-
-# %%
-# 1. 將數據轉為 DataFrame 格式，因為 TabularPreprocessor 通常需要 DataFrame
-numerical_columns = [f'num_{i}' for i in range(len(numerical_features))]
-categorical_columns = [f'cat_{i}' for i in range(len(categorical_features))]
-
-train_df_train_csv = pd.DataFrame({
-    **{f'num_{i}': X_num_train[:, i] for i in range(X_num_train.shape[1])},
-    **{f'cat_{i}': X_cat_train[:, i] for i in range(X_cat_train.shape[1])},
-    'target': y_train
-})
-
-test_df_train_csv = pd.DataFrame({
-    **{f'num_{i}': X_num_test[:, i] for i in range(X_num_test.shape[1])},
-    **{f'cat_{i}': X_cat_test[:, i] for i in range(X_cat_test.shape[1])},
-    'target': y_test
-})
-
-# %%
-train_df_train_csv.head()
-
-# %%
-train_target_df = train_df_train_csv['target']
-train_df_train_csv = train_df_train_csv.drop(columns=['target'])
-test_target_df = test_df_train_csv['target']
-test_df_train_csv = test_df_train_csv.drop(columns=['target'])
-
-# 使用 TabularPreprocessor 進行預處理
-preprocessor = TabularPreprocessor(
-    cat_features=categorical_columns,  # 類別特徵
-    numeric_features=numerical_columns,    # 數值特徵
-    normalization='standard',           # 數值特徵標準化（可選：'minmax' 或 None）
-    onehot_max_cat_num=1,
-)
-
-preprocessor.fit(train_df_train_csv)
-
-# %%
-train_df_train_csv
-
-# %%
-train_df_train_csv_processed = preprocessor.transform(train_df_train_csv)
-test_df_train_csv_processed = preprocessor.transform(test_df_train_csv)
-
-# %%
-train_df_train_csv_processed = train_df_train_csv_processed.loc[:, ~train_df_train_csv_processed.columns.duplicated(keep='last')]
-test_df_train_csv_processed = test_df_train_csv_processed.loc[:, ~test_df_train_csv_processed.columns.duplicated(keep='last')]
-
-numerical_features_processed = preprocessor.get_numeric_features()
-embedding_features_processed = preprocessor.get_embedding_features()
+        self.train_df[TARGET] = self.train_df[TARGET].astype('category')
 
 
-# %%
-train_df_train_csv_processed['target'] = train_target_df
-test_df_train_csv_processed['target'] = test_target_df
+    def _split_train_test(self):
+        # 轉成 numpy
+        X_num = self.train_df[NUMERICAL_FEATURES].values.astype(np.float32)
+        X_cat = self.train_df[CATEGORICAL_FEATURES].values
+        y = self.train_df[TARGET].values.astype(np.int64)
 
-# === 3. 建立 TabularDataset ===
-ds_train = TabularDataset(
-    data=train_df_train_csv_processed,
-    task='classification',
-    target=['target'],
-    continuous_cols=numerical_features_processed,
-    categorical_cols=embedding_features_processed
-)
+        # 分割訓練測試資料
+        return train_test_split(
+            X_num, X_cat, y, test_size=0.2, random_state=42
+        )
 
-ds_test = TabularDataset(
-    data=test_df_train_csv_processed,
-    task='classification',
-    target=['target'],
-    continuous_cols=numerical_features_processed,
-    categorical_cols=embedding_features_processed
-)
 
-dl_train = DataLoader(ds_train, batch_size=128, shuffle=False, num_workers=0, pin_memory=False)
+    def preprocess(self):
+        X_num_train, X_num_test, X_cat_train, X_cat_test, y_train, y_test = self._split_train_test()
 
-dl_test = DataLoader(ds_test, batch_size=128, shuffle=False, num_workers=0, pin_memory=False)
+        train_df_train_csv = pd.DataFrame({
+            **{f'num_{i}': X_num_train[:, i] for i in range(X_num_train.shape[1])},
+            **{f'cat_{i}': X_cat_train[:, i] for i in range(X_cat_train.shape[1])},
+            'target': y_train
+        })
 
-# %%
-print("categorical features (cols):", embedding_features_processed)
-print("categorical features (array):", train_df_train_csv_processed[embedding_features_processed].shape[1])
+        test_df_train_csv = pd.DataFrame({
+            **{f'num_{i}': X_num_test[:, i] for i in range(X_num_test.shape[1])},
+            **{f'cat_{i}': X_cat_test[:, i] for i in range(X_cat_test.shape[1])},
+            'target': y_test
+        })
 
-print("numerical features (cols):", numerical_features_processed)
-print("numerical features (array):", train_df_train_csv_processed[numerical_features_processed].shape[1])
+        self.train_target_df = train_df_train_csv['target']
+        self.test_target_df = test_df_train_csv['target']
 
-# %% [markdown]
-# # 建立模型
+        self.train_df_train_csv = train_df_train_csv.drop(columns=['target'])
+        self.test_df_train_csv = test_df_train_csv.drop(columns=['target'])
 
-# %%
-model_config = FTTransformerConfig(
-    # ModelConfig 參數
-    task="classification",  # 二元分類
-    num_attn_blocks=3,
-)
+        self.preprocessor.fit(train_df_train_csv)
+        train_df_train_csv_processed = self.preprocessor.transform(train_df_train_csv)
+        test_df_train_csv_processed = self.preprocessor.transform(test_df_train_csv)
 
-config = model_config.merge_dataset_config(ds_train)
+        self.train_df_train_csv_processed = train_df_train_csv_processed.loc[:, ~train_df_train_csv_processed.columns.duplicated(keep='last')]
+        self.test_df_train_csv_processed = test_df_train_csv_processed.loc[:, ~test_df_train_csv_processed.columns.duplicated(keep='last')]
 
-# %%
-# 初始化模型
-model = FTTransformerModel(config)
+    def set_feautres_processed(self):
+        self.numerical_features_processed = self.preprocessor.get_numeric_features()
+        self.embedding_features_processed = self.preprocessor.get_embedding_features()
+        self.train_df_train_csv_processed['target'] = self.train_target_df
+        self.test_df_train_csv_processed['target'] = self.test_target_df
 
-# %% [markdown]
-# # 訓練模型
+    def set_tablar_dataset(self):
+        # === 建立 TabularDataset ===
+        self.ds_train = TabularDataset(
+            data=self.train_df_train_csv_processed,
+            task='classification',
+            target=['target'],
+            continuous_cols=self.numerical_features_processed,
+            categorical_cols=self.embedding_features_processed
+        )
 
-# %%
-# 設置優化器和損失函數
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-criterion = torch.nn.CrossEntropyLoss()
+        self.ds_test = TabularDataset(
+            data=self.test_df_train_csv_processed,
+            task='classification',
+            target=['target'],
+            continuous_cols=self.numerical_features_processed,
+            categorical_cols=self.embedding_features_processed
+        )
 
-# %%
-# 訓練模型
-def train_model(model: FTTransformerModel, train_loader: DataLoader, criterion: torch.nn.CrossEntropyLoss, optimizer: torch.optim.Adam, num_epochs:int=20, device:str=device):
-    model.to(device)
-    model.train()
+        self.dl_train = DataLoader(self.ds_train, batch_size=128, shuffle=False, num_workers=0, pin_memory=False)
+        self.dl_test = DataLoader(self.ds_test, batch_size=128, shuffle=False, num_workers=0, pin_memory=False)
 
-    progress_bar = tqdm(range(num_epochs), leave=False)
-    for epoch in progress_bar:
-        total_loss = 0
-        for batch in train_loader:
-            numerical = batch['continuous'].to(device)
-            categorical = batch['categorical'].to(device)
-            target = batch['target'].to(device).squeeze()
-            optimizer.zero_grad()
-            outputs = model({'continuous': numerical, 'categorical': categorical})
-            logits = outputs['logits']
-            loss = criterion(logits, target)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
+        print("categorical features (cols):", self.embedding_features_processed)
+        print("categorical features (array):", self.train_df_train_csv_processed[self.embedding_features_processed].shape[1])
+        print("numerical features (cols):", self.numerical_features_processed)
+        print("numerical features (array):", self.train_df_train_csv_processed[self.numerical_features_processed].shape[1])
+
+    def set_model_config(self):
+        # 建立模型
+        self.config = self.model_config.merge_dataset_config(self.ds_train)
+        # 初始化模型
+        self.model = FTTransformerModel(self.config)
+
+    def train_model(self, num_epochs: int):
+        # 訓練模型
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
+        criterion = torch.nn.CrossEntropyLoss()
+
+        self.model.to(device)
+        self.model.train()
+
+        progress_bar = tqdm(range(num_epochs), leave=False)
+        for epoch in progress_bar:
+            total_loss = 0
+            for batch in self.dl_train:
+                numerical = batch['continuous'].to(device)
+                categorical = batch['categorical'].to(device)
+                target = batch['target'].to(device).squeeze()
+                optimizer.zero_grad()
+                outputs = self.model({'continuous': numerical, 'categorical': categorical})
+                logits = outputs['logits']
+                loss = criterion(logits, target)
+                loss.backward()
+                optimizer.step()
+                total_loss += loss.item()
+            
+            progress_bar.set_description(f"Epoch {epoch+1}/{num_epochs} | Avg. Loss: {total_loss/len(self.dl_train):.4f}")
         
-        progress_bar.set_description(f"Epoch {epoch+1}/{num_epochs} | Avg. Loss: {total_loss/len(train_loader):.4f}")
-    
-    # 儲存模型（建議用 .pt 或 .pth）
-    torch.save(model.state_dict(), 'ft_transformer_model.pth')
+        # 儲存模型（建議用 .pt 或 .pth）
+        torch.save(self.model.state_dict(), 'ft_transformer_model.pth')
 
 
-# 評估模型
-def evaluate_model(model, test_loader, device=device):
-    model.eval()
-    predictions = []
-    true_labels = []
-    with torch.no_grad():
-        for batch in test_loader:
-            numerical = batch['continuous'].to(device)
-            categorical = batch['categorical'].to(device)
-            target = batch['target'].to(device).squeeze()
-            outputs = model({'continuous': numerical, 'categorical': categorical})
-            _, preds = torch.max(outputs['logits'], dim=1)
-            predictions.extend(preds.cpu().numpy())
-            true_labels.extend(target.cpu().numpy())
-    accuracy = accuracy_score(true_labels, predictions)
-    print(f'Test Accuracy: {accuracy:.4f}')
-    print('\nClassification Report:')
-    print(classification_report(true_labels, predictions))
+    # 評估模型
+    def evaluate_model(self):
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        
+        self.model = FTTransformerModel(config=self.config)
+        self.model.load_state_dict(torch.load("ft_transformer_model.pth"))
+        self.model.to(device=device)        
+        self.model.eval()
+        
+        predictions = []
+        true_labels = []
+        with torch.no_grad():
+            for batch in self.dl_test:
+                numerical = batch['continuous'].to(device)
+                categorical = batch['categorical'].to(device)
+                target = batch['target'].to(device).squeeze()
+                outputs = self.model({'continuous': numerical, 'categorical': categorical})
+                _, preds = torch.max(outputs['logits'], dim=1)
+                predictions.extend(preds.cpu().numpy())
+                true_labels.extend(target.cpu().numpy())
+        accuracy = accuracy_score(true_labels, predictions)
+        print(f'Test Accuracy: {accuracy:.4f}')
+        print('\nClassification Report:')
+        print(classification_report(true_labels, predictions))
 
-# 執行訓練和評估
-train_model(model, dl_train, criterion, optimizer, num_epochs=100)
-evaluate_model(model, dl_test)
+    def test_csv_model_eval(self):
+        # 將類別特徵轉為 category 類型
+        for col in CATEGORICAL_FEATURES:
+            self.test_df_test_csv[col] = self.test_df_test_csv[col].astype('category')
 
-# %% [markdown]
-# # 測試集
+        X_num_test_csv = self.test_df_test_csv[NUMERICAL_FEATURES].values.astype(np.float32)
+        X_cat_test_csv = self.test_df_test_csv[CATEGORICAL_FEATURES].values
 
-# %%
-test_df_test_csv = pd.read_csv("data/test_df.csv")
+        self.test_df_test_csv = pd.DataFrame({
+            **{f'num_{i}': X_num_test_csv[:, i] for i in range(X_num_test_csv.shape[1])},
+            **{f'cat_{i}': X_cat_test_csv[:, i] for i in range(X_cat_test_csv.shape[1])},
+        })
 
-# 將類別特徵轉為 category 類型
-for col in categorical_features:
-    test_df_test_csv[col] = test_df_test_csv[col].astype('category')
+        test_df_test_csv_processed = self.preprocessor.transform(self.test_df_test_csv)
+        test_df_test_csv_processed = test_df_test_csv_processed.loc[:, ~test_df_test_csv_processed.columns.duplicated(keep='last')]
 
-# %%
-X_num_test_csv = test_df_test_csv[numerical_features].values.astype(np.float32)
-X_cat_test_csv = test_df_test_csv[categorical_features].values
+        ds_test_test_csv = TabularDataset(
+            data=test_df_test_csv_processed,
+            task='classification',
+            continuous_cols=self.numerical_features_processed,
+            categorical_cols=self.embedding_features_processed
+        )
 
-# %%
-test_df_test_csv = pd.DataFrame({
-    **{f'num_{i}': X_num_test_csv[:, i] for i in range(X_num_test_csv.shape[1])},
-    **{f'cat_{i}': X_cat_test_csv[:, i] for i in range(X_cat_test_csv.shape[1])},
-})
+        dl_test_test_csv = DataLoader(ds_test_test_csv, batch_size=128, shuffle=False, num_workers=0, pin_memory=False)
+        
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# %%
-len(test_df_test_csv)
+        # 初始化模型
+        self.model = FTTransformerModel(config=self.config)
+        self.model.load_state_dict(torch.load("ft_transformer_model.pth"))
+        self.model.to(device)
 
-# %%
-test_df_test_csv.head()
+        predictions = []
 
-# %%
-test_df_test_csv_processed = preprocessor.transform(test_df_test_csv)
-test_df_test_csv_processed = test_df_test_csv_processed.loc[:, ~test_df_test_csv_processed.columns.duplicated(keep='last')]
+        # evaluation mode
+        self.model.eval()
+        with torch.no_grad():
+            for batch in dl_test_test_csv:
+                numerical = batch['continuous'].to(device)
+                categorical = batch['categorical'].to(device)
+                outputs = self.model({'continuous': numerical, 'categorical': categorical})
+                _, preds = torch.max(outputs['logits'], dim=1)
+                predictions.extend(preds.cpu().numpy())
 
-ds_test_test_csv = TabularDataset(
-    data=test_df_test_csv_processed,
-    task='classification',
-    continuous_cols=numerical_features_processed,
-    categorical_cols=embedding_features_processed
-)
+        submission_df = pd.DataFrame({
+            "Patient_ID": range(1, len(predictions)+1),
+            "readmitted": predictions
+        })
 
-dl_test_test_csv = DataLoader(ds_test_test_csv, batch_size=128, shuffle=False, num_workers=0, pin_memory=False)
-
-# %%
-print("categorical features (cols):", embedding_features_processed)
-print("categorical features (array):", test_df_test_csv_processed[embedding_features_processed].shape[1])
-
-print("numerical features (cols):", numerical_features_processed)
-print("numerical features (array):", test_df_test_csv_processed[numerical_features_processed].shape[1])
-
-# %%
-# 12. 設置優化器和損失函數
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-criterion = torch.nn.CrossEntropyLoss()
-
-# 初始化模型
-model = FTTransformerModel(config=config)
-model.load_state_dict(torch.load("ft_transformer_model.pth"))
-model.to(device)
-
-predictions = []
-true_labels = []
-# evaluation mode
-model.eval()
-with torch.no_grad():
-    for batch in dl_test_test_csv:
-        numerical = batch['continuous'].to(device)
-        categorical = batch['categorical'].to(device)
-        outputs = model({'continuous': numerical, 'categorical': categorical})
-        _, preds = torch.max(outputs['logits'], dim=1)
-        predictions.extend(preds.cpu().numpy())
-
-submission_df = pd.DataFrame({
-    "Patient_ID": range(1, len(predictions)+1),
-    "readmitted": predictions
-})
-
-submission_df.to_csv(path_or_buf='data/submission_df.csv', index=False)
+        submission_df.to_csv(path_or_buf='data/submission_df.csv', index=False)
 
 
 
